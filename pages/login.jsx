36cce278dcfd1b58105275ab5f888ffe018c5a9a
Mehdi-ehsani <metti.work@gmail.com>
2024-11-09 15:21:30 +0330

    Added login and register pages

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,9 +1,76 @@
-import React from 'react'
+import Link from "next/link";
+import Image from 'next/image';
 
-const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+import useLoginReducer from "../store/reducers/useLoginReducer";
+import validateLoginForm from "../utils/validateLoginForm";
+import { setCookie } from "../utils/cookie";
+import { useLogin } from "../services/mutations";
+import logo from "../assets/image/logo.png";
+import styles from "./Forms.module.css";
+import { useRouter } from "next/router";
 
-export default Login
+
+const LoginPage = () => {
+	const [formData, dispatchFormData] = useLoginReducer();
+	const { mutate } = useLogin();
+	const router = useRouter();
+
+	const changeHandler = (event) => {
+		const value = event.target.value;
+		const name = event.target.name.toUpperCase();
+
+		switch (name) {
+			case "NAME":
+				dispatchFormData({ type: "NAME", payload: value });
+				break;
+			case "PASSWORD":
+				dispatchFormData({ type: "PASSWORD", payload: value });
+		}
+	};
+	const loginUser = (event) => {
+		event.preventDefault();
+		if (validateLoginForm(formData, dispatchFormData)) {
+			mutate(
+				{ username: formData.name, password: formData.password },
+				{
+					onSuccess: (data) => {
+						console.log(data.data);
+						setCookie("token", data.data?.token);
+						router.replace("/")
+					},
+					onError: (error) => console.log(error.response.data.message),
+				}
+			);
+		}
+	};
+	return (
+		<div className={styles.container}>
+			<form className={styles.form}>
+			<Image src={logo} alt="logo" className={styles.logo}/>
+				<h1>فرم ورود</h1>
+				<input
+					onChange={changeHandler}
+					name="name"
+					value={formData.name}
+					type="text"
+					placeholder="نام کاربری"
+				/>
+				<p className={styles.error}>{formData.errors.name}</p>
+				<input
+					onChange={changeHandler}
+					name="password"
+					value={formData.password}
+					type="password"
+					placeholder="رمز عبور"
+				/>
+				<p className={styles.error}>{formData.errors.password}</p>
+				<button onClick={loginUser} className={styles.submitBtn} type="submit">
+					ورود
+				</button>
+				<Link href={"/register"} >ایجاد حساب کاربری!</Link>
+			</form>
+		</div>
+	);
+};
+
+export default LoginPage;
--- pages/register.jsx
+++ pages/register.jsx
@@ -1,9 +1,93 @@
-import React from 'react'
+import { useEffect } from "react";
+import Link from "next/link";
+import Image from 'next/image';
 
-const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
 
-export default Register
+
+import useRegisterReducer from "../store/reducers/useRegisterReducer";
+import validateRegisterForm from "../utils/validateRegisterForm";
+import { useRegister } from "../services/mutations";
+import { getCookie } from "../utils/cookie";
+import styles from "./Forms.module.css";
+import logo from "../assets/image/logo.png";
+
+const RegistrationPage = () => {
+	const [formData, dispatchFormData] = useRegisterReducer();
+	const { mutate } = useRegister();
+	// const navigate = useNavigate();
+
+	// useEffect(() => {
+	// 	const token = getCookie("token");
+	// 	token && navigate("/");
+	// }, [navigate]);
+
+	const changeHandler = (event) => {
+		const value = event.target.value;
+		const name = event.target.name.toUpperCase();
+
+		switch (name) {
+			case "NAME":
+				dispatchFormData({ type: "NAME", payload: value });
+				break;
+			case "PASSWORD":
+				dispatchFormData({ type: "PASSWORD", payload: value });
+				break;
+			case "CONFIRM_PASSWORD":
+				dispatchFormData({ type: "CONFIRM_PASSWORD", payload: value });
+		}
+	};
+
+	const addUser = (event) => {
+		event.preventDefault();
+		if (validateRegisterForm(formData, dispatchFormData)) {
+			mutate(
+				{ username: formData.name, password: formData.password },
+				{
+					onSuccess: (data) => {
+						console.log(data.data.message);
+						// navigate("/login");
+					},
+					onError: (error) => console.log(error.response.data.message),
+				}
+			);
+		}
+	};
+	return (
+		<div className={styles.container}>
+			<form className={styles.form}>
+				<Image className={styles.logo} src={logo} alt="logo" />
+				<h1>فرم ثبت نام</h1>
+				<input
+					onChange={changeHandler}
+					name="name"
+					value={formData.name}
+					type="text"
+					placeholder="نام کاربری"
+				/>
+				<p className={styles.error}>{formData.errors.name}</p>
+				<input
+					onChange={changeHandler}
+					name="password"
+					value={formData.password}
+					type="password"
+					placeholder="رمز عبور"
+				/>
+				<p className={styles.error}>{formData.errors.password}</p>
+				<input
+					onChange={changeHandler}
+					name="confirm_password"
+					value={formData.confirmPassword}
+					type="password"
+					placeholder=" تکرار رمز عبور"
+				/>
+				<p className={styles.error}>{formData.errors.confirmPassword}</p>
+				<button onClick={addUser} className={styles.submitBtn} type="submit">
+					ثبت نام
+				</button>
+				<Link href="/login">حساب کاربری دارید؟</Link>
+			</form>
+		</div>
+	);
+};
+
+export default RegistrationPage;
