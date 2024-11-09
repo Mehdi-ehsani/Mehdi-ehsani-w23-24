3dce2309603bf2da142aa368bdb43bc68c664bba
Mehdi-ehsani <metti.work@gmail.com>
2024-11-09 20:08:10 +0330

    Added notFound page

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import Image from "next/image"
+import Link from "next/link"
+
+import styles from "./404.module.css"
+import img from "../assets/image/notfound.png"
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div className={styles.container}>
+      <Image src={img} alt="404 logo"/>
+      <Link href="/">بازگشت به صفحه اصلی</Link>
+    </div>
   )
 }
 
