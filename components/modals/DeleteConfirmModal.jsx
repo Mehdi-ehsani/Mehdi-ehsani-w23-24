1f460f8b60c0221eeda798b7c0ff59de5610c573
Mehdi-ehsani <metti.work@gmail.com>
2024-11-09 15:17:59 +0330

    Added delete Modal Added modal to confirm delete product

---
+++ components/modals/DeleteConfirmModal.jsx
@@ -0,0 +1,34 @@
+import { useDeleteProduct } from "../../services/mutations";
+import Image from 'next/image';
+
+import closeImg from "../../assets/image/close.png";
+import styles from "./DeleteConfirmModal.module.css"
+
+const DeleteConfirmModal = ({ id, setIsDeleteModalShow }) => {
+	const { mutate } = useDeleteProduct();
+
+    const deleteHandler = () => {
+		const data = { ids: [id] };
+		mutate(
+			{ data },
+			{
+				onSuccess: (data) => console.log(data),
+				onError: (error) => console.log(error),
+			}
+		);
+	};
+	return (
+		<div className={styles.container} onClick={() => setIsDeleteModalShow(false)}>
+			<div className={styles.form} onClick={(e) => e.stopPropagation()}>
+				<Image src={closeImg} alt="icon" />
+				<h2>آیا از حذف این محصول مطمئنید؟</h2>
+				<div className={styles.btnContainer}>
+					<button className={styles.delete} onClick={deleteHandler}>حذف</button>
+					<button className={styles.close} onClick={() => setIsDeleteModalShow(false)}>لغو</button>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default DeleteConfirmModal;
