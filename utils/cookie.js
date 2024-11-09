88d1687bfdee88de0d22ca5497da325867d9ef64
Mehdi-ehsani <metti.work@gmail.com>
2024-11-09 16:01:03 +0330

    Fix cookie bug in next document is undefiend in ssr

--- utils/cookie.js
+++ utils/cookie.js
@@ -1,12 +1,17 @@
 function setCookie(name, value) {
+  if (typeof document !== 'undefined') {
     const maxAge = 30 * 24 * 60 * 60;
     document.cookie = `${name}=${value}; max-age=${maxAge}; path=/`;
   }
-  
-  function getCookie(name) {
-    const value = `; ${document?.cookie}`;
-    const parts = value?.split(`; ${name}=`);
-    if (parts?.length === 2) return parts?.pop()?.split(";")?.shift();
+}
+
+function getCookie(name) {
+  if (typeof document !== 'undefined') {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
   }
-  
-  export { setCookie, getCookie };
+  return null;
+}
+
+export { setCookie, getCookie };
